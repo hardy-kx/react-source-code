5a3b25c51a61c44905f66f3c5d3a1e0bd0a9426f
zkx <991992844@qq.com>
2022-03-31 16:17:45 +0800

    修改字体大小

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React1
+          Learn React
         </a>
       </header>
     </div>
